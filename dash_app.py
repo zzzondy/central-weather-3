d151078db6c3979aaf55a1d12143b9cef5856dd5
Artyom Rodionov <artemr19032006@gmail.com>
2024-10-31 19:31:35 +0300

    Небольшие визуальные улучшения

--- dash_app.py
+++ dash_app.py
@@ -39,16 +39,17 @@ app.layout = dbc.Container([
                 width=6)
     ]),
     dbc.Row([
-        html.H6('День на карте', style={'margin-top': '20px'}),
+        html.H6('Выберите день для отображения информации на карте:', style={'margin-top': '20px'}),
         dbc.Col(dcc.Dropdown(
             id='day-selection',
-            options=[{'label': 'День 1', 'value': 1}],
+            options=[{'label': 'День 1', 'value': 0}],
             value=0,
             clearable=False,
             className='form-control'
-        ), width=6, style={'margin-top': '20px', }),
+        ), width=6, style={'margin-top': '20px'}),
     ]),
     html.H2(id='travel-text', style={'margin-top': '20px', 'text-align': 'center'}),
+    html.Div(id='cities-not-found', children=[], style={'margin-top': '20px', 'text-align': 'center',}),
     html.Div(id='graphs-container'),
     dl.Map(id='weather-map', style={'height': '50vh'}, center=[55.7558, 37.6173], zoom=6, children=[
         dl.TileLayer()
@@ -88,17 +89,21 @@ def update_day_selection(forecast_days):
     Output('output-container', 'children'),
     Output('travel-text', 'children'),
     Output('weather-map', 'children'),
+    Output('cities-not-found', 'children'),
     Input('submit-button', 'n_clicks'),
     Input('forecast-days', 'value'),
     Input('day-selection', 'value'),
+    Input('add-city-button', 'n_clicks'),
 )
-def update_graph(n_clicks, forecast_days, selected_day):
+def update_graph(n_clicks, forecast_days, selected_day, n_clicks_add):
     forecast_days = int(forecast_days)
 
     if n_clicks > 0:
         graphs = []
         messages = []
         markers = []
+        cities_not_found = []
+        cities_not_found_plain_text = []
 
         if not cities:
             return [], "Введите хотя бы один город.", "", []
@@ -117,7 +122,7 @@ def update_graph(n_clicks, forecast_days, selected_day):
                                                 params={'lat': lat, 'lon': lon})
                 weather_data = weather_response.json()
 
-                # Создание графика для каждого города
+                # Создание графиков для каждого города
                 temp_data = []
                 wind_speed_data = []
                 humidity_data = []
@@ -134,34 +139,86 @@ def update_graph(n_clicks, forecast_days, selected_day):
                     humidity_data.append(humidity_day)
                     rain_data.append(rain_volume_day)
 
-                # Создание графиков
+                # Создание графиков для температуры
                 graphs.append(
                     dcc.Graph(
                         figure={
                             'data': [
-                                go.Scatter(x=[f'Day {i + 1}' for i in range(forecast_days)], y=temp_data,
+                                go.Scatter(x=[f'День {i + 1}' for i in range(forecast_days)], y=temp_data,
                                            mode='lines+markers', name=f'Температура ({city}) (°C)',
                                            hoverinfo='text',
-                                           text=[f'День {i + 1}: {temp}°C' for i, temp in enumerate(temp_data)]),
-                                go.Scatter(x=[f'Day {i + 1}' for i in range(forecast_days)], y=wind_speed_data,
+                                           text=[f'День {i + 1}: {temp}°C' for i, temp in enumerate(temp_data)],
+                                           line=dict(color='red'))  # Цвет графика температуры
+                            ],
+                            'layout': go.Layout(
+                                title=f'Температура в {city}',
+                                xaxis_title='Дни',
+                                yaxis_title='Температура (°C)',
+                                legend=dict(x=0.5, y=1.0)
+                            )
+                        }
+                    )
+                )
+
+                # Создание графиков для скорости ветра
+                graphs.append(
+                    dcc.Graph(
+                        figure={
+                            'data': [
+                                go.Scatter(x=[f'День {i + 1}' for i in range(forecast_days)], y=wind_speed_data,
                                            mode='lines+markers', name=f'Скорость ветра ({city}) (км/ч)',
                                            hoverinfo='text',
                                            text=[f'День {i + 1}: {wind} км/ч' for i, wind in
-                                                 enumerate(wind_speed_data)]),
-                                go.Scatter(x=[f'Day {i + 1}' for i in range(forecast_days)], y=humidity_data,
+                                                 enumerate(wind_speed_data)],
+                                           line=dict(color='blue'))  # Цвет графика скорости ветра
+                            ],
+                            'layout': go.Layout(
+                                title=f'Скорость ветра в {city}',
+                                xaxis_title='Дни',
+                                yaxis_title='Скорость ветра (км/ч)',
+                                legend=dict(x=0.5, y=1.0)
+                            )
+                        }
+                    )
+                )
+
+                # Создание графиков для влажности
+                graphs.append(
+                    dcc.Graph(
+                        figure={
+                            'data': [
+                                go.Scatter(x=[f'День {i + 1}' for i in range(forecast_days)], y=humidity_data,
                                            mode='lines+markers', name=f'Влажность ({city}) (%)',
                                            hoverinfo='text',
                                            text=[f'День {i + 1}: {humidity}%' for i, humidity in
-                                                 enumerate(humidity_data)]),
-                                go.Scatter(x=[f'Day {i + 1}' for i in range(forecast_days)], y=rain_data,
-                                           mode='lines+markers', name=f'Осадки ({city}) (мм)',
-                                           hoverinfo='text',
-                                           text=[f'День {i + 1}: {rain} мм' for i, rain in enumerate(rain_data)])
+                                                 enumerate(humidity_data)],
+                                           line=dict(color='green'))  # Цвет графика влажности
                             ],
                             'layout': go.Layout(
-                                title=f'Погода в {city}',
+                                title=f'Влажность в {city}',
                                 xaxis_title='Дни',
-                                yaxis_title='Значения',
+                                yaxis_title='Влажность (%)',
+                                legend=dict(x=0.5, y=1.0)
+                            )
+                        }
+                    )
+                )
+
+                # Создание графиков для осадков
+                graphs.append(
+                    dcc.Graph(
+                        figure={
+                            'data': [
+                                go.Scatter(x=[f'День {i + 1}' for i in range(forecast_days)], y=rain_data,
+                                           mode='lines+markers', name=f'Осадки ({city}) (мм)',
+                                           hoverinfo='text',
+                                           text=[f'День {i + 1}: {rain} мм' for i, rain in enumerate(rain_data)],
+                                           line=dict(color='orange'))  # Цвет графика осадков
+                            ],
+                            'layout': go.Layout(
+                                title=f'Осадки в {city}',
+                                xaxis_title='Дни',
+                                yaxis_title='Осадки (мм)',
                                 legend=dict(x=0.5, y=1.0)
                             )
                         }
@@ -177,16 +234,22 @@ def update_graph(n_clicks, forecast_days, selected_day):
                                    f'Осадки: {rain_data[selected_day]} мм\n'
                                    f'День: {selected_day + 1}')
                     ]))
-                else:
-                    messages.append(f"Выбранный день за пределами доступных данных для города {city}.")
+
             else:
-                messages.append(f"Город {city} не найден.")
+                cities_not_found.append(
+                    html.H6(f"Город {city} не найден", style={'margin-top': '20px', 'text-align': 'center'})
+                )
+                cities_not_found_plain_text.append(city)
 
-        travel_text_content = f"Путешествие через: {', '.join(cities)}" if cities else ""
+        travel_text_content = f"Путешествие через: {', '.join(filter_cities(cities, cities_not_found_plain_text))}" if cities else ""
 
-        return graphs, "<br>".join(messages), travel_text_content, [dl.TileLayer()] + markers
+        return graphs, "<br>".join(messages), travel_text_content, [dl.TileLayer()] + markers, cities_not_found
 
-    return [], "", "", []
+    return [], "", "", [], []
+
+
+def filter_cities(cities, correct_cities):
+    return [city for city in cities if city not in correct_cities]
 
 
 if __name__ == '__main__':
